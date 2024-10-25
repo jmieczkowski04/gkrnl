c8a60f43ccb47dbcbbddf6d515cbb9bee06714a7
Jakub Mieczkowski <jmieczkowski04@gmail.com>
2024-10-25 07:31:27 +0200

    Implemented boot error handling

--- init/init.c
+++ init/init.c
@@ -1,10 +1,17 @@
+#include "gkrnl/primitives.h"
+#include "boot/multiboot.h"
 #include "drivers/vga/vga.h"
 
 
 
-void vm_boot32_main()
+void vm_boot32_main(uint32_t mb_magic, void* mb_info_phys_addr)
 {
     vga_init();
+    if (mb_magic != MULTIBOOT_BOOTLOADER_MAGIC)
+    {
+        vga_print("Kernel boot error!\n");
+        return;
+    }
     vga_print("Sonne!");
     for(;;);
     return;
